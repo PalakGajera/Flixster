f2ee26356ae2af60c6a1b5ba99efa7a012f3cc8a
PalakGajera <pgajera@uci.edu>
2019-10-17 09:44:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/palak/flixster/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.palak.flixster;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v4.content.ContextCompat;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
