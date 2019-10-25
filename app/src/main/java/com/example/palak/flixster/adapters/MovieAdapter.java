c559a0ae49fcd7f9cbaaa42ddfb1ecb9434f10e1
PalakGajera <pgajera@uci.edu>
2019-10-25 13:23:35 -0700

    Added Recycler View and Alternative Layout. I also changed the UI from green backround to pink and the image, title and description and title are also in the center.

--- app/src/main/java/com/example/palak/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/palak/flixster/adapters/MovieAdapter.java
@@ -1,17 +1,22 @@
 package com.example.palak.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.palak.flixster.DetailActivity;
 import com.example.palak.flixster.R;
 import com.example.palak.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -54,6 +59,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -64,9 +70,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie)
+        public void bind(final Movie movie)
         {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
@@ -83,6 +90,23 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             // then imageUrl = back drop image
             // else image = poster_image
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // 1. Register click listener on the whole row
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new activity on tap
+//                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+//                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+
+
+                }
+            });
         }
     }
 }
